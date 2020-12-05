fe8efa3b667410f447bb72503984c467b15fa082
kronenberg <kronenberg@auxmoney.com>
2020-12-05 19:45:56 +0100

    Added color to stats container

--- src/component/ValueBox.js
+++ src/component/ValueBox.js
@@ -10,8 +10,14 @@ export default class ValueBox extends React.Component {
     }
 
     render() {
+      let color = 'green';
+      if (this.props.cases7Per100k >= 200) {
+        color = 'red';
+      } else if (this.props.cases7Per100k >= 100) {
+        color = 'orange';
+      }
       return (
-        <Statistic size='huge'>
+        <Statistic size='huge' color={color}>
           <Statistic.Label>{this.props.city}</Statistic.Label>
           <p>Daten vom: {this.props.lastUpdated === "" ? "unbekannt" : dayjs(this.props.lastUpdated).format('dd. DD.MM.YYYY')}</p>
           <Statistic.Value>{Math.round(this.props.cases7Per100k)}</Statistic.Value>
