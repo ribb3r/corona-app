fbc462baf5e6102d1596a09b3f79e2748e3ee7c4
kronenberg <kronenberg@auxmoney.com>
2020-12-08 19:48:46 +0100

    Added loading layer

--- src/api/covid-19.js
+++ src/api/covid-19.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+// https://public.opendatasoft.com/explore/dataset/covid-19-germany-landkreise/api
 const httpClient = axios.create({
     baseURL: 'https://public.opendatasoft.com/api/records/1.0/search/'
 })
--- src/component/ValueBoxGroup.js
+++ src/component/ValueBoxGroup.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Container, Button, Icon, Grid, Header } from 'semantic-ui-react'
+import { trackPromise, promiseTrackerHoc } from 'react-promise-tracker';
+import { Container, Dimmer, Loader, Button, Icon, Grid, Header } from 'semantic-ui-react'
 import ValueBox from "./ValueBox";
 import api from '../api/covid-19'
 
-export default class ValueBoxGroup extends React.Component {
+class ValueBoxGroup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -31,51 +32,65 @@ export default class ValueBoxGroup extends React.Component {
     const cities = ["essen", "krefeld", "solingen"];
     cities.forEach ((city) => {
       const params = new URLSearchParams([['q', city]]);
-      api.getCasesForOneWeek(params).then(response => response.data.records[0].fields)
-      .then((data) => {
-        let updatedCity = {};
-        updatedCity.cases7Per100k = data.cases7_per_100k;
-        updatedCity.lastUpdated = data.last_update;
-        this.setState({ [`${city}`]: updatedCity })
-       })
-      .catch(error => {
-        console.error(error)
-      });
-    });
-  }
+      trackPromise(
+        api.getCasesForOneWeek(params).then(response => response.data.records[0].fields)
+          .then((data) => {
+            let updatedCity = {};
+            updatedCity.cases7Per100k = data.cases7_per_100k;
+            updatedCity.lastUpdated = data.last_update;
+            this.setState({ [`${city}`]: updatedCity })
+          })
+          .catch(error => {
+            console.error(error)
+          })
+      );
+  })}
 
   handleClick = () => {
     this.queryApi();
   };
 
   render() {
+    let mainContent;
+    if (this.props.promiseInProgress) {
+      mainContent = <div>
+        <Dimmer active inverted>
+          <Loader inverted size='large'>Wird geladen</Loader>
+        </Dimmer>
+      </div>;
+    } else {
+      mainContent = <div className='ui centered grid'>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column>
+                <ValueBox city="Essen" cases7Per100k={this.state.essen.cases7Per100k} lastUpdated={this.state.essen.lastUpdated}/>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <ValueBox city="Krefeld" cases7Per100k={this.state.krefeld.cases7Per100k} lastUpdated={this.state.krefeld.lastUpdated}/>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+              <ValueBox city="Solingen" cases7Per100k={this.state.solingen.cases7Per100k} lastUpdated={this.state.solingen.lastUpdated}/>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+      </div>
+    }
+    
     return (
       <Container>
-          <Header as='h1'>COVID-19-Erkrankung</Header>
-          <Header as='h5' dividing>pro 100.000 Einwohner in den letzten 7 Tagen</Header>
-          <Button icon className="ui right floated button" title="aktualisieren" onClick={this.handleClick}>
-            <Icon name="refresh" />
-          </Button>
-          <div className='ui centered grid'>
-            <Grid>
-              <Grid.Row>
-                <Grid.Column>
-                  <ValueBox city="Essen" cases7Per100k={this.state.essen.cases7Per100k} lastUpdated={this.state.essen.lastUpdated}/>
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row>
-                <Grid.Column>
-                  <ValueBox city="Krefeld" cases7Per100k={this.state.krefeld.cases7Per100k} lastUpdated={this.state.krefeld.lastUpdated}/>
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row>
-                <Grid.Column>
-                <ValueBox city="Solingen" cases7Per100k={this.state.solingen.cases7Per100k} lastUpdated={this.state.solingen.lastUpdated}/>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-        </div>
-      </Container>
-    );
-  }
+        <Header as='h1'>COVID-19-Erkrankung</Header>
+        <Header as='h5' dividing>pro 100.000 Einwohner in den letzten 7 Tagen</Header>
+        <Button icon className="ui right floated button" title="aktualisieren" onClick={this.handleClick}>
+          <Icon name="refresh" />
+        </Button>
+        {mainContent}
+        </Container>
+      )
+    };
 }
+
+export default promiseTrackerHoc(ValueBoxGroup);
